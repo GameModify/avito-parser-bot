9d30764b526bd11d8361fde711d0c3dca67f7848
Serent <Serent2003@mail.ru>
2025-04-22 17:28:40 +0300

    feat: added cookie to requests

--- config.py
+++ config.py
@@ -7,9 +7,15 @@ URLS = [
 FILE_PATH = 'avito_ads.jsonl'
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)...",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
     "Accept": "application/json, text/plain, */*",
     "Referer": "https://www.avito.ru/",
+    "Origin": "https://www.avito.ru",
+    "Accept-Language": "ru-RU,ru;q=0.9",
+}
+
+COOKIES = {
+    "sessid": "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwIjoyMzY5MjYxNSwidiI6ZmFsc2UsImV4dHJhIjpudWxsLCJleHAiOjE3NDU0MTU5MTIsImlhdCI6MTc0NTMyOTUxMiwicyI6IjQ4MDE1ZDVmZWFiNDRmY2U3MjgzNjhhMjA3NDc4MGJhLjE3NDUzMjk1MTIiLCJoIjoiZTVjNTViMTZmODkxZGZlNDBjZGJhYTg2M2ViYzkzZmZfMTc0NTMyOTUxMiIsInUiOjEzMDUwNDkxNX0.z3ntdwnnYCcq4lmW8ljdiW-WANlY_zmDPCfqemJBYvXIU0-5IGJ8bc_3Rg9-WW-bn7rEp8dfE1zgANJktV58Ig"
 }
 
 FETCH_INTERVAL = 180
--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from random import randrange
 from asyncio import run
 from aiohttp import ClientSession
 
-from config import URLS, HEADERS, FILE_PATH, FETCH_INTERVAL, SEEN_ADS_FILE
+from config import URLS, HEADERS, COOKIES, FILE_PATH, FETCH_INTERVAL, SEEN_ADS_FILE
 from scraper import process_url
 from storage.seen_ads import load_seen_ads, save_seen_ads
 from utils import countdown
 
 
 async def main():
-    seen_ads = load_seen_ads(SEEN_ADS_FILE)
-    async with ClientSession(headers=HEADERS) as session:
+    seen_ads = await load_seen_ads(SEEN_ADS_FILE)
+    async with ClientSession(headers=HEADERS, cookies=COOKIES) as session:
         while True:
             for url in URLS:
                 await process_url(
@@ -18,10 +18,9 @@ async def main():
                     base_url=url,
                     file_path=FILE_PATH,
                     seen_ads=seen_ads,
-                    page_delay=10
+                    page_delay=4
                 )
-            save_seen_ads(seen_ads, SEEN_ADS_FILE)
-            timer = randrange(FETCH_INTERVAL, FETCH_INTERVAL + 40   )
+            timer = randrange(FETCH_INTERVAL, FETCH_INTERVAL + 40  )
             await countdown(timer)
 
 
